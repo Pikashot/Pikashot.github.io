25ce74c48526c8d04360ca07d508223f5296134c
Maxime Laplace <maxime.laplace29@gmail.com>
2022-10-10 19:37:33 +0200

    script

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+
+const POINTS_ODJ = [
+    "point numero 1",
+    "point numero 2",
+    "point numero 3",
+    "point numero 4",
+]
+
+const BLAGUES = [
+    "blague 1",
+    "blague 2",
+    "blague 3",
+    "blague 4",
+]
+
+const donnerPoint = () => {
+    if (Math.random() > 0.8 || POINTS_ODJ.length === 0) {
+        alert(BLAGUES[Math.floor(Math.random()*BLAGUES.length)])
+    } else {
+        const index = Math.floor(Math.random()*POINTS_ODJ.length)
+        const point = POINTS_ODJ[index]
+        POINTS_ODJ.splice(index)
+        alert(point)
+    }
+}
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -9,10 +35,16 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
 
+    const element = document.getElementById(data)
+
+    ev.target.appendChild(element);
 
     if (ev.target.id === data + '-container') {
-        console.log('yes')
+        element.draggable = false
+        element.onmouseenter = () => {}
+        ev.target.style.border = '3px solid lightgreen'
+        ev.target.style.borderRadius = '15px'
+        setTimeout(donnerPoint, 200)
     }
 }
